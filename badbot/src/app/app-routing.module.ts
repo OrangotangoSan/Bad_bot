f05d3b0254c3294baebd23902d74e28fe6280b46
José Joaquim <josejoaquimrizzo@hotmail.com>
2020-07-24 20:07:32 +0100

    v2

--- badbot/src/app/app-routing.module.ts
+++ badbot/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { HomeComponent } from './comeco/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- badbot/src/app/app.module.ts
+++ badbot/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { HomeComponent } from './comeco/home/home.component';
+import { NavComponent } from './comeco/nav/nav.component';
+import { FooterComponent } from './comeco/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
